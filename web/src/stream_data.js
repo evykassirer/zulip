8eeb17d654327fe2a6fa0b34ec9d5affdb935b73
evykassirer <evy@zulip.com>
2023-08-05 15:00:41 -0700

    stream_data: Get is_invite_only by stream_id instead of name.

--- web/src/stream_data.js
+++ web/src/stream_data.js
@@ -634,8 +634,8 @@ export function is_web_public(stream_id) {
     return sub !== undefined && sub.is_web_public;
 }
 
-export function is_invite_only_by_stream_name(stream_name) {
-    const sub = get_sub(stream_name);
+export function is_invite_only_by_stream_id(stream_id) {
+    const sub = get_sub_by_id(stream_id);
     if (sub === undefined) {
         return false;
     }
