39448662b81a77d9a036257ca5d3dcc01c4b9e92
evykassirer <evy@zulip.com>
2024-10-23 23:10:56 -0700

    buddy_list: Add option to view avatar in buddy list.
    Fixes #19999.
    
    This change adds an option to show users' avatar and
    status in the buddy list. Previous options are still
    available in the settings menu.

--- web/src/settings.js
+++ web/src/settings.js
@@ -91,8 +91,10 @@ export function update_lock_icon_in_sidebar() {
 export function build_page() {
     setup_settings_label();
 
+    console.log(people.small_avatar_url_for_person(people.get_by_user_id(people.my_current_user_id())))
     const rendered_settings_tab = render_settings_tab({
         full_name: people.my_full_name(),
+        profile_picture: people.small_avatar_url_for_person(people.get_by_user_id(people.my_current_user_id())),
         date_joined_text: get_parsed_date_of_joining(),
         current_user,
         page_params,
