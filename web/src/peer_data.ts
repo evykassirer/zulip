98037a11299a1915915a1512c773b45e83eb5d0e
Evy Kassirer <evy@zulip.com>
2025-04-15 22:52:37 -0700

    peer_data: Add ability to retry failed subscriber fetch.

--- web/src/peer_data.ts
+++ web/src/peer_data.ts
@@ -8,6 +8,7 @@ import {page_params} from "./page_params.ts";
 import type {User} from "./people.ts";
 import * as people from "./people.ts";
 import * as sub_store from "./sub_store.ts";
+import * as util from "./util.ts";
 
 // This maps a stream_id to a LazySet of user_ids who are subscribed.
 const stream_subscribers = new Map<number, LazySet>();
@@ -35,14 +36,19 @@ const fetch_stream_subscribers_response_schema = z.object({
     subscribers: z.array(z.number()),
 });
 
-export async function maybe_fetch_stream_subscribers(stream_id: number): Promise<LazySet | null> {
+export async function maybe_fetch_stream_subscribers(
+    stream_id: number,
+    retry_on_failure = true,
+    num_attempts = 1,
+): Promise<LazySet | null> {
     if (pending_subscriber_requests.has(stream_id)) {
         return pending_subscriber_requests.get(stream_id)!.subscribers_promise;
     }
     const subscribers_promise = (async () => {
         let subscribers: number[];
+        let xhr: JQuery.jqXHR<unknown> | undefined;
         try {
-            const xhr = await channel.get({
+            xhr = await channel.get({
                 url: `/json/streams/${stream_id}/members`,
             });
             subscribers = fetch_stream_subscribers_response_schema.parse(xhr).subscribers;
@@ -51,6 +57,12 @@ export async function maybe_fetch_stream_subscribers(stream_id: number): Promise
                 stream_id,
             });
             pending_subscriber_requests.delete(stream_id);
+            if (retry_on_failure && xhr) {
+                num_attempts += 1;
+                const retry_delay_secs = util.get_retry_backoff_seconds(xhr, num_attempts);
+                await new Promise((resolve) => setTimeout(resolve, retry_delay_secs));
+                return maybe_fetch_stream_subscribers(stream_id, true, num_attempts);
+            }
             return null;
         }
 
@@ -93,12 +105,18 @@ function get_loaded_subscriber_subset(stream_id: number): LazySet {
     return subscribers;
 }
 
-async function get_full_subscriber_set(stream_id: number): Promise<LazySet | null> {
+async function get_full_subscriber_set(
+    stream_id: number,
+    retry_on_failure = true,
+): Promise<LazySet | null> {
     assert(!page_params.is_spectator);
     // This function parallels `get_loaded_subscriber_subset` but ensures we include all
     // subscribers, possibly fetching that data from the server.
     if (!fetched_stream_ids.has(stream_id)) {
-        const fetched_subscribers = await maybe_fetch_stream_subscribers(stream_id);
+        const fetched_subscribers = await maybe_fetch_stream_subscribers(
+            stream_id,
+            retry_on_failure,
+        );
         // This means a request failed and we don't know who the subscribers are.
         if (fetched_subscribers === null) {
             return null;
@@ -266,8 +284,9 @@ export function is_user_subscribed(stream_id: number, user_id: number): boolean 
 export async function maybe_fetch_is_user_subscribed(
     stream_id: number,
     user_id: number,
+    retry_on_failure = true,
 ): Promise<boolean | null> {
-    const subscribers = await get_full_subscriber_set(stream_id);
+    const subscribers = await get_full_subscriber_set(stream_id, retry_on_failure);
     // This means the request failed. We will return `null` here if
     // we can't determine if this user is subscribed or not.
     if (subscribers === null) {
